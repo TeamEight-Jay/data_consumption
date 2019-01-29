cedcf076ee1c816ddb74895f1bd2a68e71ec3f8b
jayjoshi <jay.joshi@coviam.com>
2019-01-29 10:14:18 +0530

    Initial commit

---
+++ src/main/java/com/recommendation/data_consumption/DataConsumptionApplication.java
@@ -0,0 +1,14 @@
+package com.recommendation.data_consumption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConsumptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataConsumptionApplication.class, args);
+	}
+
+}
+
